648ad5d0a0e887ef9d596449feb74ca639d0a555
Karen Lo <just.karenlo@gmail.com>
2014-12-08 00:06:00 -0800

    updated index page and removed incomplete project

---
+++ stylesheets/mysite.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  setInterval(function(){
+    $('#lf1').fadeOut(2000);
+    $('#lf1').fadeIn(2000);
+
+    $('#rf2').fadeOut(2000);
+    $('#rf2').fadeIn(2000);
+
+    $('#lf2').fadeOut(2000);
+    $('#lf2').fadeIn(2000);
+
+    $('#rf1').fadeOut(2000);
+    $('#rf1').fadeIn(2000);
+  }, 0);
+
+// setInterval(function(){
+//     $('#lf2').fadeOut('8000');
+//     $('#lf2').fadeIn('8000');
+
+//     $('#rf1').fadeOut('8000');
+//     $('#rf1').fadeIn('8000');
+
+//   }, 1000);
+
+})
